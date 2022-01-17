cee90e6fa433b13e5e3fc7eac3bd0d1a7c0280e7
unknown <natnaeldenbi1@gmail.com>
2022-01-17 22:21:13 +0300

    models and controllers added

---
+++ root/src/main/java/com/gada/root/PostsRepositary.java
@@ -0,0 +1,7 @@
+package com.gada.root;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostsRepositary extends CrudRepository<Posts,Long>  {
+    
+}
--- root/src/main/java/com/gada/root/RootApplication.java
+++ root/src/main/java/com/gada/root/RootApplication.java
@@ -1,7 +1,11 @@
 package com.gada.root;
 
+import java.time.LocalDateTime;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RootApplication {
@@ -9,5 +13,12 @@ public class RootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RootApplication.class, args);
 	}
+	// @Bean
+	// public CommandLineRunner dataRunner(PostsRepositary repositary){
+	// 	LocalDateTime ldt = LocalDateTime.now();
+	// 	return args -> {
+	// 		repositary.save(new Posts(Long.valueOf(1),"The GERD","Fund for the Great Ethiopian Renesence dam",ldt));
+	// 	};
+	// }
 
 }
