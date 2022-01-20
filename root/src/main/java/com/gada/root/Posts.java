6cea6e485097b8a3081f27e4c25615827799ca50
babi <yaedt29@gmail.com>
2022-01-20 22:33:35 +0300

    login and registration

---
+++ root/src/main/java/com/gada/root/Posts.java
@@ -0,0 +1,57 @@
+package com.gada.root;
+
+import java.time.LocalDateTime;
+// import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+// import javax.persistence.Transient;
+// import javax.persistence.Lob;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+// import javax.validation.constraints.NotEmpty;
+// import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+// import org.springframework.format.annotation.NumberFormat;
+// import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+// import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(columnDefinition = "MEDIUMBLOB")
+
+    private byte[] ProductImg;
+    @Column(columnDefinition = "LONGVARCHAR")
+    private String Base64Img;
+
+    @NotNull
+    @NotBlank(message = "*Description is required!")
+    private String description;
+    @NotNull
+    @NotBlank(message = "*Title is required!")
+    private String title;
+
+    @NotNull(message = "*Account Number Cannot be null!")
+    @Min(value = 999, message = "*Account Number is invalid!")
+    private Long accountNumber;
+    private LocalDateTime date;
+    // @ManyToOne
+    // private Client client;
+
+}
--- root/src/main/java/com/gada/root/RootApplication.java
+++ root/src/main/java/com/gada/root/RootApplication.java
@@ -1,9 +1,18 @@
 package com.gada.root;
 
+<<<<<<< HEAD
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+=======
+// import java.time.LocalDateTime;
+
+// import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.Bean;
+>>>>>>> 4630ea08f13bd7d904524b688a683648380bcb97
 
 @SpringBootApplication
 public class RootApplication {
@@ -11,6 +20,13 @@ public class RootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RootApplication.class, args);
 	}
+	// @Bean
+	// public CommandLineRunner dataRunner(PostsRepositary repositary){
+	// 	LocalDateTime ldt = LocalDateTime.now();
+	// 	return args -> {
+	// 		repositary.save(new Posts(Long.valueOf(1),"The GERD","Fund for the Great Ethiopian Renesence dam",ldt));
+	// 	};
+	// }
 
 // 	@Bean
 // 	public CommandLineRunner dataLoader(UserRepository repo) {
