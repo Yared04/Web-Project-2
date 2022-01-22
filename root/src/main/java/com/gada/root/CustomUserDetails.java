6aca28edcbded54cb9f49943ae3e70a40d46dcfd
unknown <natnaeldenbi1@gmail.com>
2022-01-22 21:11:38 +0300

    style modified

--- root/src/main/java/com/gada/root/CustomUserDetails.java
+++ root/src/main/java/com/gada/root/CustomUserDetails.java
@@ -35,31 +35,31 @@ public class CustomUserDetails  implements UserDetails{
 
     @Override
     public String getUsername() {
-        // TODO Auto-generated method stub
+        
         return user.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
+        
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
+        
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
+
         return true;
     }
 
     @Override
     public boolean isEnabled() {
-        // TODO Auto-generated method stub
+        
         return true;
     }
     
